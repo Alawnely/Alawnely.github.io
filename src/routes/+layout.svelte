48db503d9f0a422bdde0cec8703a843f682f9357
Alawnely <15603444+Alawnely@users.noreply.github.com>
2024-08-12 20:26:44 +0800

    Migrate to SvelteKit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		min-height: 100%;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Alawnely's Default Website</title>
+	<meta name="description" content="Alawnely's Website" />
+</svelte:head>
+
+<h1>Hi there 👋</h1>
+
+<style>
+	h1 {
+		margin: auto;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
